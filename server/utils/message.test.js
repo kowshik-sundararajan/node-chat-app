483cecf85eb578284f9094c474d0541487ba0bc4
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-12-10 11:37:55 +0530

    Added mustache.js for templating

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -17,7 +17,7 @@ describe('generateLocationMessage', () => {
         var from = 'Kow';
         var latitude = 15;
         var longitude = 19;
-        var url = 'https://www.google.com/maps?q=15,19';
+        var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
         var message = generateLocationMessage(from, latitude, longitude);
 
         expect(typeof message.createdAt).toBe('number');
